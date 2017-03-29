b7a3445a7edc076b7ca1eecde29c65f6297052bd
Jesus Guerra <jguerrix@gmail.com>
2017-03-29 14:16:41 -0700

    Add empty framework project

---
+++ DataKit/DataKit.h
@@ -0,0 +1,19 @@
+//
+//  DataKit.h
+//  DataKit
+//
+//  Created by Jesus Guerra on 3/29/17.
+//  Copyright © 2017 Jesus Guerra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataKit.
+FOUNDATION_EXPORT double DataKitVersionNumber;
+
+//! Project version string for DataKit.
+FOUNDATION_EXPORT const unsigned char DataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataKit/PublicHeader.h>
+
+
