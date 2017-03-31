0bfe0b0826b8a8b9ca26e7c80b33e44f67f38cec
Jesus Guerra <jguerrix@gmail.com>
2017-03-30 18:10:06 -0700

    Update contact data model

--- DataKit/ContactDataModel.swift
+++ DataKit/ContactDataModel.swift
@@ -10,27 +10,40 @@ import Foundation
 import RealmSwift
 
 public extension Contact {
-  
-  /// Persist or update record into database
-  public func saveOrUpdate() {
-    let realm = try! Realm()
-    try! realm.write {
-      realm.add(self, update: true)
+    
+    /// Persist or update record into database
+    public func saveOrUpdate() {
+        if id == 0 {
+            self.id = Int(NSDate().timeIntervalSince1970) // Generate id from TimeStam
+        }
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(self, update: true)
+        }
     }
-  }
-  
-  /// Delete record from database
-  public func delete() {
-    let realm = try! Realm()
-    try! realm.write {
-      realm.delete(self)
+    
+    /// Delete record from database
+    public func delete() {
+        let realm = try! Realm()
+        try! realm.write {
+            realm.delete(self)
+        }
     }
-  }
-  
-  /// Get All Contacts from database
-  public static func getAllContacts() -> Results<Contact> {
-    let realm = try! Realm()
-    return realm.objects(Contact.self)
-  }
-  
+    
+    
+    /// Get All Contacts from database
+    ///
+    /// - Returns: Results<Contact> generic array for with Contact objects
+    public static func getAll() -> Results<Contact> {
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        let realm = try! Realm()
+        return realm.objects(Contact.self)
+    }
+    
+    public static func getAllWithoutGroup() -> Results<Contact> {
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        let realm = try! Realm()
+        return realm.objects(Contact.self).filter("group == nil")
+    }
+    
 }
